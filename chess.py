bab313891934499762f8f8c57fa69cff6aaa601f
vincentchen18 <vincent.y.chen2011@gmail.com>
2026-06-14 11:42:58 +1000

    add ruy lopez variations

--- chess.py
+++ chess.py
@@ -323,7 +323,23 @@ def build_opening_book():
                       f(("g1", "f3")), f(("b8", "c6")),
                       f(("f1", "b5")), f(("a7", "a6")),
                       f(("b5", "a4")), f(("g8", "f6")),
-                      f(("e1", "g1")), f(("f1", "e1"))], book) # ruy lopez
+                      f(("e1", "g1")), f(("f6", "e4")),
+                      f(("d2", "d4")), f(("b7", "b5")),
+                      f(("a4", "b3")), f(("d7", "d5"))], book) # ruy lopez
+    book = lineadder([f(("e2", "e4")), f(("e7", "e5")),
+                      f(("g1", "f3")), f(("b8", "c6")),
+                      f(("f1", "b5")), f(("a7", "a6")),
+                      f(("b5", "a4")), f(("f8", "e7")),
+                      f(("e1", "g1")), f(("g8", "f6")),
+                      f(("f1", "e1")), f(("b7", "b5")),
+                      f(("a4", "b3")), f(("e8", "g8"))], book)  # ruy lopez again!
+    book = lineadder([f(("e2", "e4")), f(("e7", "e5")),
+                      f(("g1", "f3")), f(("b8", "c6")),
+                      f(("f1", "b5")), f(("a7", "a6")),
+                      f(("b5", "a4")), f(("g8", "f6")),
+                      f(("e1", "g1")), f(("f8", "e7")),
+                      f(("f1", "e1")), f(("b7", "b5")),
+                      f(("a4", "b3")), f(("e8", "g8"))], book)  # ruy lopez again!
     book = lineadder([f(("e2", "e4")), f(("e7", "e5")),
                       f(("b1", "c3")), f(("g8", "f6")),
                       f(("f2", "f4")), f(("d7", "d5")),
@@ -366,6 +382,7 @@ def build_opening_book():
                       f(("b1", "c3")), f(("g8", "f6")),
                       f(("g2", "g3")), f(("d7", "d6")),
                       f(("f1", "g2"))], book) # english opening: reversed sicilian dragon variation
+    #"""
 
 
 
