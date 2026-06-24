113ab0d1aa6aa490c27cfc1a906f018a69d08954
vincentchen18 <vincent.y.chen2011@gmail.com>
2026-06-24 19:23:53 +1000

    more openings and more opening bugfixes for mdoern

--- chess.py
+++ chess.py
@@ -420,7 +420,7 @@ def build_opening_book():
                       f(("d2", "d4")), f(("f8", "g7")),
                       f(("b1", "c3")), f(("c7", "c5")),
                       f(("d4", "c5")), f(("g7", "c3")),
-                      f(("b1", "c3"))], book)  # modern defense
+                      f(("b2", "c3"))], book)  # modern defense
     book = lineadder([f(("e2", "e4")), f(("c7", "c6")),
                       f(("d2", "d4")), f(("d7", "d5")),
                       f(("e4", "e5")), f(("c6", "c5")),
@@ -437,11 +437,16 @@ def build_opening_book():
                       f(("d2", "d4")), f(("d7", "d5")),
                       f(("e4", "e5")), f(("c8", "f5")),
                       f(("g1", "f3")), f(("e7", "e6"))], book) #carokann
+    book = lineadder([f(("e2", "e4")), f(("c7", "c6")),
+                      f(("d2", "d4")), f(("d7", "d5")),
+                      f(("b1", "d2")), f(("d5", "e4")),
+                      f(("d2", "e4")), f(("g8", "f6"))], book) # more carokann
     book = lineadder([f(("e2", "e4")), f(("c7", "c5")),
                       f(("c2", "c3")), f(("d7", "d5")),
                       f(("e4", "e5")), f(("d5", "d4")),
                       f(("g1", "f3")), f(("b8", "c6")),
-                      f(("f1", "b5"))], book)
+                      f(("f1", "b5"))], book) #alapin sicilian
+
 
     return book
 
